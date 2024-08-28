f3d9114cef45da812c62806d13cba89fed9224af
jhonanova3 <88212725+jhonanova3@users.noreply.github.com>
2024-08-28 02:14:13 -0300

    Traer todos los Productos de un Negocio

--- api/src/models/productoModel.js
+++ api/src/models/productoModel.js
@@ -1,28 +1,36 @@
-// src/models/producto.js
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   sequelize.define('Producto', {
     id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+      type: DataTypes.UUID,               // tipo de dato UUID para una identificación única y universal.
+      defaultValue: DataTypes.UUIDV4,     // Asigna automáticamente un valor UUIDv4 si no se proporciona uno.
+      primaryKey: true,                   //  clave primaria del modelo.
     },
     nombre: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      type: DataTypes.STRING,             
+      allowNull: false,                   // Indica que este campo es obligatorio, no puede ser nulo.
     },
     descripcion: {
-      type: DataTypes.TEXT,
-      allowNull: false,
+      type: DataTypes.TEXT,               // texto largo
+      allowNull: false,                 
     },
     precio: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
+      type: DataTypes.FLOAT,              // número decimal (FLOAT).
+      allowNull: false,                   // 
     },
-    imagen: { // Agregamos la nueva columna
-      type: DataTypes.STRING,
-      allowNull: true, // Permitir nulos al principio
+    imagen: {
+      type: DataTypes.STRING,             //  cadena de texto para almacenar la URL de la imagen.
+      allowNull: true,                    // Este campo es opcional, puede ser nulo.
+    },
+    //agregando columna negocio_id que será una clave foránea apuntando al modelo Negocio.
+    negocio_id: {
+      type: DataTypes.UUID,               // Define el tipo de dato como UUID para relacionarlo con el negocio asociado.
+      allowNull: false,                   // Este campo es obligatorio, cada producto debe estar asociado a un negocio.
+      references: {
+        model: 'Negocios',                // Especifica que esta columna se refiere a la columna 'id' del modelo relacionado 'Negocios'.
+        key: 'id',                        // La clave foránea apunta a la columna 'id' de la tabla 'Negocios'.
+      },
     },
   });
 };
