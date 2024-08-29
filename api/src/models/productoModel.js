e5ef47932b1ce08ecc62837d8f6be608b2f7b1af
jhonanova3 <88212725+jhonanova3@users.noreply.github.com>
2024-08-28 22:00:39 -0300

    Solucionando errores para los endpoints

--- api/src/models/productoModel.js
+++ api/src/models/productoModel.js
@@ -3,32 +3,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   sequelize.define('Producto', {
     id: {
-      type: DataTypes.UUID,               // tipo de dato UUID para una identificación única y universal.
+      type: DataTypes.UUID,               // Tipo de dato UUID para una identificación única y universal.
       defaultValue: DataTypes.UUIDV4,     // Asigna automáticamente un valor UUIDv4 si no se proporciona uno.
-      primaryKey: true,                   //  clave primaria del modelo.
+      primaryKey: true,                   // Establece este campo como la clave primaria del modelo.
     },
     nombre: {
       type: DataTypes.STRING,             
       allowNull: false,                   // Indica que este campo es obligatorio, no puede ser nulo.
     },
     descripcion: {
-      type: DataTypes.TEXT,               // texto largo
-      allowNull: false,                 
+      type: DataTypes.TEXT,               // Campo de tipo texto largo para la descripción del producto.
+      allowNull: false,                   
     },
     precio: {
-      type: DataTypes.FLOAT,              // número decimal (FLOAT).
-      allowNull: false,                   // 
+      type: DataTypes.FLOAT,              // Tipo FLOAT para almacenar precios con decimales.
+      allowNull: false,                   
     },
     imagen: {
-      type: DataTypes.STRING,             //  cadena de texto para almacenar la URL de la imagen.
+      type: DataTypes.STRING,             // Cadena de texto para almacenar la URL de la imagen.
       allowNull: true,                    // Este campo es opcional, puede ser nulo.
     },
-    //agregando columna negocio_id que será una clave foránea apuntando al modelo Negocio.
+    // Definición de la columna negocio_id como clave foránea apuntando al modelo Negocio.
     negocio_id: {
       type: DataTypes.UUID,               // Define el tipo de dato como UUID para relacionarlo con el negocio asociado.
       allowNull: false,                   // Este campo es obligatorio, cada producto debe estar asociado a un negocio.
       references: {
-        model: 'Negocios',                // Especifica que esta columna se refiere a la columna 'id' del modelo relacionado 'Negocios'.
+        model: sequelize.models.Negocio,  // Cambio: Usando el modelo directamente para evitar errores de nombres.
         key: 'id',                        // La clave foránea apunta a la columna 'id' de la tabla 'Negocios'.
       },
     },
