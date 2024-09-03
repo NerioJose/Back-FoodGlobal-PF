1ddf6ede107bf15ba8bb20b59204951f35fdec22
NerioJose <neriojose531@gmail.com>
2024-09-03 00:57:40 -0500

    Se resuelve conflicto routes index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,49 +1,47 @@
 const express = require('express');
 const routes = express.Router();
 
+
 // Importar todos los controladores 
-// Get Controllers
+
+//Get Controllers
 const getDetailNegocios = require('../controllers/getControllers/getDetailNegocios');
 const getDetailProductos = require('../controllers/getControllers/getDetailProductos');
 const getNegocios = require('../controllers/getControllers/getNegocios');
 const getProductos = require('../controllers/getControllers/getProductos');
 const getUsuarios = require('../controllers/getControllers/getUsuarios');
-const getProductosPorNegocio = require('../controllers/getControllers/getProductosPorNegocio'); 
+const getProductosPorNegocio = require('../controllers/getControllers/getProductosPorNegocio'); // Importa el controlador de productos por negocio
 
 
-
-// Post Controllers
+//Post Controllers
 const postNegocios = require('../controllers/postControllers/postNegocios');
 const postProductos = require('../controllers/postControllers/postProductos');
 const postUsuarios = require('../controllers/postControllers/postUsuarios');
-const loginUsuario = require('../controllers/postControllers/loginUsuario'); 
 const paymentIntent = require('../controllers/paymentControllers/paymentController');
-
-// Delete Controller
-const deleteUsuario = require('../controllers/deleteControllers/deleteUsuario');
+const loginUsuario = require('../controllers/postControllers/loginUsuario');
 const deleteProducto = require('../controllers/deleteControllers/deleteProducto');
-const deleteNegocio = require('../controllers/deleteControllers/deleteNegocio');
-const auth = require('../middleware/authentication');
+const deleteUsuario = require('../controllers/deleteControllers/deleteUsuario');
+
 
 // Configurar las rutas
-
-
 routes.get('/negocios', getNegocios); // Obtener la lista de negocios
 routes.get('/negocios/:id', getDetailNegocios); // Obtener detalles de un negocio específico
 routes.get('/productos', getProductos); // Obtener la lista de productos
 routes.get('/productos/:id', getDetailProductos); // Obtener detalles de un producto específico
-routes.get('/usuarios', auth(['admin']), getUsuarios); // Obtener la lista de usuarios solo si se tiene rol 'admin'
+routes.get('/usuarios', getUsuarios); // Obtener la lista de usuarios
+
+// Nueva ruta para obtener productos por negocio
 routes.get('/negocios/:negocioId/productos', getProductosPorNegocio); // Obtener todos los productos de un negocio específico
 
-routes.post('/negocios', auth(['admin', 'socio']), postNegocios); // Crear Negocios solo si se tiene rol 'admin' o 'socio'
-routes.post('/productos', auth(['admin', 'socio']), postProductos); // Crear Productos solo si se tiene rol 'admin' o 'socio'
-routes.post('/usuarios', postUsuarios); // Crear Usuarios (No requiere autenticación)
-routes.post('/login', loginUsuario); // Login de usuarios (No requiere autenticación)
-routes.post('/create-payment-intent', paymentIntent); // Ruta para la pasarela Stripe
+routes.post('/negocios', postNegocios);// Crear Negocios
+routes.post('/productos', postProductos);//Crear Productos
+routes.post('/usuarios', postUsuarios);//Crear usuarios
+routes.post('/create-payment-intent', paymentIntent);//Ruta para la pasarela stripe
+routes.post('/login', loginUsuario )
 
-routes.delete('/usuarios/:id', auth(['admin']), deleteUsuario); // Eliminar usuario solo si se tiene rol 'admin'
-routes.delete('/productos/:id', auth(['admin', 'socio']), deleteProducto); // Eliminar producto solo si se tiene rol 'admin' o 'socio'
-routes.delete('/negocios/:id', auth(['admin', 'socio']), deleteNegocio); // Eliminar negocio solo si se tiene rol 'admin' o 'socio'
+routes.delete('/:id', deleteProducto);
+routes.delete('/usuarios/:id', deleteUsuario); 
 
-module.exports = routes;
+module.exports = routes
+
 
