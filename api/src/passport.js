12c2838f01e26ede9030e070e4b26eea638f668d
NerioJose <neriojose531@gmail.com>
2024-09-10 21:43:44 -0500

    Se modifica URL de callback en el archivo passport.js

--- api/src/passport.js
+++ api/src/passport.js
@@ -9,12 +9,10 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
-
-
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3001/auth/google/callback" // Debe coincidir con el URI de redireccionamiento autorizado
+  callbackURL: 'https://back-foodglobal-pf.up.railway.app/auth/google/callback' // Debe coincidir con el URI de redireccionamiento autorizado
 },
   async (accessToken, refreshToken, profile, done) => {
     console.log(profile);
